127f5720c89e4866d59f322d07f071feb96aab75
sunil agrawal <sunil@affordableseos.com>
2017-06-22 11:59:27 +0530

    small change

--- test-project/ViewController.swift
+++ test-project/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hello World")
+        print("Hello World ....")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
